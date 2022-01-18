2cfc063fd474ff4eadafffaed8445a46935c090e
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-01-18 13:31:36 +0000

    Set root page for authenticated and unauthenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  root 'devise/sessions#new', as: :unauthenticated_root
 
   resources :posts
 end
