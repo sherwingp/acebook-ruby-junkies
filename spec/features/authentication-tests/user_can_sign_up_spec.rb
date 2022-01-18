52aba640b2f29f4d3f4fc4fc02daf4fd99f540c9
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-01-18 15:53:04 +0000

    configured controller to accept name and surname params

--- spec/features/authentication-tests/user_can_sign_up_spec.rb
+++ spec/features/authentication-tests/user_can_sign_up_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.feature "Signing up", type: :feature do
   scenario "User can sign up" do
     visit "/users/sign_up"
+
+		fill_in "user[name]", with: "John"
+		fill_in "user[surname]", with: "Doe"
     fill_in "user[email]", with: "test@test.com"
 		fill_in "user[password]", with: "123456"
 		fill_in "user[password_confirmation]", with: "123456"
     click_button "Sign up"
+		
+		expect(page).to have_content("Signed in as: John")
 		expect(page).to have_content('Welcome! You have signed up successfully.')
   end
 end
