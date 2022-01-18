a721141ef4a94ac67c4ddd7f30c5f83861eb332d
Hereloss <Hereloss@googlemail.com>
2022-01-18 16:37:03 +0000

    Image test added

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -10,4 +10,13 @@ RSpec.feature "Timeline", type: :feature do
     #Database has username as name
     expect(page).to have_content("name")
   end
+
+  scenario "should have an image once an image post is made" do
+    visit "/posts/new"
+    fill_in "Message", with: "Hello, world!"
+    attach_file('post[image]',
+                File.join(Rails.root, '/spec/image1.jpeg'), :visible => false)
+    click_button "Post"
+    page.should have_css('img')
+  end
 end
