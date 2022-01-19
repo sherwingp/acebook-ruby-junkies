be87d15d483c3469013a5dd21c43b1811907fcce
Chris Thomas <hereloss@googlemail.com>
2022-01-19 16:29:50 +0000

    Posting bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   root "posts#index"
 
     get "/posts", to: "posts#index"
-    get "/posts/:id", to: "posts#show"
   
   resources :posts do
     resources :comments
