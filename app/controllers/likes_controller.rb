7da6811ed49f7c49e540f9becfd595fb20e8893e
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-01-19 15:49:28 +0000

    Add redirect to liked post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,10 @@
 class LikesController < ApplicationController
-
+  include ActionView::RecordIdentifier
+  
   def create
     @post = Post.find(params[:post_id])
     @post.likes.create(user_id: current_user.id)
-    redirect_to post_path(@post)
+    redirect_to posts_path(@post, anchor: dom_id(@post))
     # anchor: ‘some_anchor’
   end
 end
