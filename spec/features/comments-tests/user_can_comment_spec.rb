e6b20b036f79943669a2214b07dd40d88c0637b6
Chris Thomas <hereloss@googlemail.com>
2022-01-19 16:51:38 +0000

    Tests added

---
+++ spec/features/comments-tests/user_can_comment_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.feature "Commenting", type: :feature do
+  scenario 'Can comment on a post' do
+    visit "/users/sign_up"
+		fill_in "user[name]", with: "John"
+		fill_in "user[surname]", with: "Doe"
+    fill_in "user[email]", with: "test@test.com"
+		fill_in "user[password]", with: "123456"
+		fill_in "user[password_confirmation]", with: "123456"
+    click_button "Sign up"
+    visit "/posts"
+    click_link "What's on your mind?"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Post"
+    expect(page).to have_content("Hello, world!")
+    visit '/posts'
+    fill_in "comment[body]", with: "A comment!"
+    click_button "Create Comment"
+    visit '/posts'
+    expect(page).to have_content("A comment!")
+  end
+
+  scenario 'Can comment on a post and it will show your name' do
+    visit "/users/sign_up"
+		fill_in "user[name]", with: "John"
+		fill_in "user[surname]", with: "Doe"
+    fill_in "user[email]", with: "test@test.com"
+		fill_in "user[password]", with: "123456"
+		fill_in "user[password_confirmation]", with: "123456"
+    click_button "Sign up"
+    visit "/posts"
+    click_link "What's on your mind?"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Post"
+    expect(page).to have_content("Hello, world!")
+    visit '/posts'
+    fill_in "comment[body]", with: "A comment!"
+    click_button "Create Comment"
+    visit '/posts'
+    expect(page).to have_content("John Doe A comment!")
+  end
+# Since we don't want them to view this page, not sure if needed?
+  # scenario 'Can comment on a post on the posts page' do
+  #   visit "/users/sign_up"
+	# 	fill_in "user[name]", with: "John"
+	# 	fill_in "user[surname]", with: "Doe"
+  #   fill_in "user[email]", with: "test@test.com"
+	# 	fill_in "user[password]", with: "123456"
+	# 	fill_in "user[password_confirmation]", with: "123456"
+  #   click_button "Sign up"
+  #   visit "/posts"
+  #   click_link "What's on your mind?"
+  #   fill_in "Message", with: "Hello, world!"
+  #   click_button "Post"
+  #   visit '/posts/1'
+  #   fill_in "comment[body]", with: "A comment!"
+  #   click_button "Create Comment"
+  #   visit '/posts'
+  #   expect(page).to have_content("A comment!")
+  # end
+end
