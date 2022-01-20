238939847d4abb6f79548856b642b04487a5a80f
Chris Thomas <hereloss@googlemail.com>
2022-01-20 10:30:01 +0000

    Latest Version;

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,10 @@
 class CommentsController < ApplicationController
-  # def create
-  #   @post = Post.find(params[:post_id])
-  #   @comment = @post.comments.create(comment_params)
-  #   @comment.user_id = current_user.id 
-  #   if @comment.save
-  #     redirect_to @post
-  #   else
-  #     # flash.now[:danger] = "error"
-  #     puts "error"
-  #   end
-  # end
-
-  # private
-  #   def comment_params
-  #     params = params.require(:comment).permit(:body)
-  #   end
 
   before_action :set_user
   before_action :set_post
 
   def create
     @comment = @post.comments.create(comment_params)
-
     if @comment.save
       redirect_to @post
     else
