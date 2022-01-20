6492b3501121095b569fb802318bb8e2464bab8c
Jessica Gordon <ketchesj@gmail.com>
2022-01-20 12:23:14 +0000

    test for adding image on a comment passing TODO: make test more specific

--- spec/features/comments-tests/user_can_comment_spec.rb
+++ spec/features/comments-tests/user_can_comment_spec.rb
@@ -40,6 +40,30 @@ RSpec.feature "Commenting", type: :feature do
     visit '/posts'
     expect(page).to have_content("John Doe A comment!")
   end
+
+  scenario 'User can add a photo to their comment' do
+    visit "/users/sign_up"
+		fill_in "user[name]", with: "John"
+		fill_in "user[surname]", with: "Doe"
+    fill_in "user[email]", with: "test@test.com"
+		fill_in "user[password]", with: "123456"
+		fill_in "user[password_confirmation]", with: "123456"
+    click_button "Sign up"
+    visit "/posts"
+    click_link "What's on your mind?"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Post"
+    expect(page).to have_content("Hello, world!")
+    visit '/posts'
+    fill_in "comment[body]", with: "A comment!"
+    attach_file('comment[image]',
+      File.join(Rails.root, '/spec/image1.jpeg'), :visible => false)
+    click_button "Create Comment"
+    #  TODO: make line 63 pass, to make test more specific
+    # expect(page).to have_xpath("//img[contains(@src, '/spec/image1.jpeg')]")
+    page.should have_css('img')
+  end
+
 # Since we don't want them to view this page, not sure if needed?
   # scenario 'Can comment on a post on the posts page' do
   #   visit "/users/sign_up"
