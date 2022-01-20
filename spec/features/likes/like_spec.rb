b13e6cb3184b7e5bb4df9db82202d3d4a3b892c9
Francesco <francescocollotto@gmail.com>
2022-01-20 11:55:40 +0000

    add rspec tests for likes

---
+++ spec/features/likes/like_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.feature "Likes", type: :feature do
+  scenario "" do
+		
+  end
+end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it('create a valid like') do
+    like = Like.create(user_id: 1, post_id: 1)
+    expect(like.user_id).to be 1
+  end
+
+  it('reject an invalid like') do
+    like = Like.create()
+    expect(like.valid?).to be_falsey
+  end
 end
