9ce54a89cfeb4e57db535bbfb5590e9cd9c76f4b
Francesco <francescocollotto@gmail.com>
2022-01-20 12:35:26 +0000

    add feature tests for like and unlike

--- spec/features/likes/like_spec.rb
+++ spec/features/likes/like_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature "Likes", type: :feature do
-  scenario "" do
-		
+  scenario "a user can like a post and unlike a post" do
+    visit "/users/sign_up"
+		fill_in "user[name]", with: "John"
+		fill_in "user[surname]", with: "Doe"
+    fill_in "user[email]", with: "test@test.com"
+		fill_in "user[password]", with: "123456"
+		fill_in "user[password_confirmation]", with: "123456"
+    click_button "Sign up"
+
+    visit '/posts/new'
+    fill_in 'post[message]', with: 'I love Rails!'
+    click_on 'Post'
+
+    click_on 'Like'
+    expect(page).to have_content '1 Like'
+    click_on 'Unlike'
+    expect(page).to have_content '0 Likes'
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'support/controllers_helpers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -32,7 +33,7 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
-
+  config.include ControllerHelpers, type: :controller
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
