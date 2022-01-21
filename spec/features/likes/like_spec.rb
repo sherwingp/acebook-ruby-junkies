8183f7e84de401dfc14c582a543f4987af881a7f
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-01-21 14:49:06 +0000

    Write test for comment like

--- spec/features/likes/like_spec.rb
+++ spec/features/likes/like_spec.rb
@@ -1,25 +1,31 @@
 require 'rails_helper'
 
 RSpec.feature "Likes", type: :feature do
-  scenario "a user can like a post" do
+  before(:each) do
     user = FactoryBot.create(:user)
     login_as(user, :scope => :user)
     post = FactoryBot.create(:post)
+  end
 
+  scenario "a user can like a post" do
     visit '/posts'
     click_on 'Like'
     expect(page).to have_content '1 Like'
   end
 
   scenario "a user can unlike a post" do
-    user = FactoryBot.create(:user)
-    login_as(user, :scope => :user)
-    post = FactoryBot.create(:post)
-    
     visit '/posts'
     click_on 'Like'
 
     click_on 'Unlike'
     expect(page).to have_content '0 Likes'
+  end
+
+  scenario "a user can like a comment" do
+    visit '/posts'
+    fill_in "comment[body]", with: "A comment!"
+    click_button "Create Comment"
+    visit '/posts'
+    click_button 'Like Comment'
   end
 end
