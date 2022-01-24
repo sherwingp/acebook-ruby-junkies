f847a88c3e9a34572a99940bdf188b2a4482f87f
Jessica Gordon <ketchesj@gmail.com>
2022-01-24 20:43:53 +0000

    validates comment body only if no image is attached

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,6 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
   has_one_attached :image, dependent: :destroy, service: :local
-  validates :body, presence: true, length: { minimum: 1 }
+
+  validates :body, presence: true, if: -> { |c| c.image.blank? }
 end
