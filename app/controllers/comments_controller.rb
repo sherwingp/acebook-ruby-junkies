59a42b195e63c537843f30c9b9e66463fba43294
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-01-24 14:57:19 +0000

    Added the functionality to edit comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,18 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to posts_path
+  end
+
 
   private
 
