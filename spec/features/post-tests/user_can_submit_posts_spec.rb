f7f30ea853693154149e08baa5052979b33cd042
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-01-25 11:18:51 +0000

    added feature tests for editing and deleting posts and additional one for editing a comment

--- spec/features/post-tests/user_can_submit_posts_spec.rb
+++ spec/features/post-tests/user_can_submit_posts_spec.rb
@@ -1,38 +1,24 @@
 require 'rails_helper'
+require_relative "../../helpers/sign_up_helper_spec.rb"
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    visit "/users/sign_up"
-		fill_in "user[name]", with: "John"
-		fill_in "user[surname]", with: "Doe"
-    fill_in "user[email]", with: "test@test.com"
-		fill_in "user[password]", with: "123456"
-		fill_in "user[password_confirmation]", with: "123456"
-    click_button "Sign up"
-
-    visit "/posts"
-    # click_link "What's on your mind?"
+    sign_up
+    visit "/posts/new"
     fill_in "post[message]", with: "Hello, world!"
-    click_button "Create Post"
+    click_on "Post"
     expect(page).to have_content("Hello, world!")
     #Database has username as name
     expect(page).to have_content("John Doe")
   end
 
   scenario "should have an image once an image post is made" do
-    visit "/users/sign_up"
-		fill_in "user[name]", with: "John"
-		fill_in "user[surname]", with: "Doe"
-    fill_in "user[email]", with: "test@test.com"
-		fill_in "user[password]", with: "123456"
-		fill_in "user[password_confirmation]", with: "123456"
-    click_button "Sign up"
-		
-    visit "/posts"
+    sign_up
+    visit "/posts/new"
     fill_in "post[message]", with: "Hello, world!"
     attach_file('post[image]',
                 File.join(Rails.root, '/spec/feature_test_image1.jpeg'), :visible => false)
-    click_button "Create Post"
+    click_button "Post"
     expect(page).to have_css("img[src*='/feature_test_image1.jpeg']")
   end
 end
