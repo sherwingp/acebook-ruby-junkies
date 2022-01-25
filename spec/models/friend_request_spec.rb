c8842befce5dab2d04336741491c65b904564979
Jessica Gordon <ketchesj@gmail.com>
2022-01-25 20:50:16 +0000

    fixed - friend request model spec not clearing test database between tests

--- spec/models/friend_request_spec.rb
+++ spec/models/friend_request_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe FriendRequest, type: :model do
   # let(:user1) { double("user1", :id => 1, :friends => []) }
   # let(:user2) { double("user2", :id => 2, :friends => []) }
   # let(:friend_request) { double("friend request", :id => 1, :user_id => 1, :friend_id => 2) }
-  
-  request = FactoryBot.create(:friend_request)
+  before(:each) do
+    @request = FactoryBot.create(:friend_request)
+  end
 
   describe('#initialize') do
     it('creates a friend request instance') do
@@ -15,9 +16,8 @@ RSpec.describe FriendRequest, type: :model do
 
   describe('#accept') do
     it('adds friend to user friends') do
-      p request
-      request.accept
-    
+      p @request
+      @request.accept
     end
 
     it('destroys friend request') do
