f9373aef87ffe1f78d3fca2f678161f3d02bdd69
Chris Thomas <hereloss@googlemail.com>
2022-01-26 12:55:39 +0000

    Most tests for profiles passing

--- spec/features/profiles-tests/can_edit_profile_spec.rb
+++ spec/features/profiles-tests/can_edit_profile_spec.rb
@@ -2,13 +2,51 @@ require 'rails_helper'
 
 RSpec.feature 'Edit Profile', type: :feature do
   scenario 'Can edit your own profile' do
+    visit '/users/sign_up'
+    fill_in 'user[name]', with: 'Kitty'
+    fill_in 'user[surname]', with: 'TheCat'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: '123456'
+    fill_in 'user[password_confirmation]', with: '123456'
+    click_button 'Sign up'
+    expect(page).to have_content('Create Profile')
+    fill_in 'profile_about', with: 'Im a cat!'
+    click_button 'Create Profile'
+    click_on 'Edit Profile'
+    fill_in 'profile_about', with: 'Im not a cat!'
+    click_button 'Edit Profile'
+    expect(page).to have_content('Im not a cat!')
   end
 
-  scenario 'Upon editing the details are changed' do
-
-  end
 
   scenario 'Cannot edit others profiles' do
-    
+    visit '/users/sign_up'
+    fill_in 'user[name]', with: 'Kit'
+    fill_in 'user[surname]', with: 'TheDog'
+    fill_in 'user[email]', with: 'testing@test.com'
+    fill_in 'user[password]', with: '123456'
+    fill_in 'user[password_confirmation]', with: '123456'
+    click_button 'Sign up'
+    expect(page).to have_content('Create Profile')
+    fill_in 'profile_about', with: 'Im a dog!'
+    click_button 'Create Profile'
+    click_on 'Sign Out'
+
+    visit '/users/sign_up'
+    fill_in 'user[name]', with: 'Kitty'
+    fill_in 'user[surname]', with: 'TheCat'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: '123456'
+    fill_in 'user[password_confirmation]', with: '123456'
+    click_button 'Sign up'
+    expect(page).to have_content('Create Profile')
+    fill_in 'profile_about', with: 'Im a cat!'
+    click_button 'Create Profile'
+    visit '/'
+    visit '/users/profiles'
+    expect(page).to have_content('Kit TheDog')
+    click_on 'Kit TheDog'
+    expect(page).not_to have_content('Edit Profile')
+
   end 
 end
--- spec/features/profiles-tests/can_view_profiles_spec.rb
+++ spec/features/profiles-tests/can_view_profiles_spec.rb
@@ -2,13 +2,97 @@ require 'rails_helper'
 
 RSpec.feature 'View Profile', type: :feature do
   scenario 'Can see a list of all active profiles' do
+    visit '/users/sign_up'
+    fill_in 'user[name]', with: 'Kit'
+    fill_in 'user[surname]', with: 'TheDog'
+    fill_in 'user[email]', with: 'testing@test.com'
+    fill_in 'user[password]', with: '123456'
+    fill_in 'user[password_confirmation]', with: '123456'
+    click_button 'Sign up'
+    expect(page).to have_content('Create Profile')
+    fill_in 'profile_about', with: 'Im a dog!'
+    click_button 'Create Profile'
+    click_on 'Sign Out'
+
+    visit '/users/sign_up'
+    fill_in 'user[name]', with: 'Kitty'
+    fill_in 'user[surname]', with: 'TheCat'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: '123456'
+    fill_in 'user[password_confirmation]', with: '123456'
+    click_button 'Sign up'
+    expect(page).to have_content('Create Profile')
+    fill_in 'profile_about', with: 'Im a cat!'
+    click_button 'Create Profile'
+    visit '/'
+    visit '/users/profiles'
+    expect(page).to have_content('Kit TheDog')
   end
 
   scenario 'Can click and view others profiles' do
+    visit '/users/sign_up'
+    fill_in 'user[name]', with: 'Kit'
+    fill_in 'user[surname]', with: 'TheDog'
+    fill_in 'user[email]', with: 'testing@test.com'
+    fill_in 'user[password]', with: '123456'
+    fill_in 'user[password_confirmation]', with: '123456'
+    click_button 'Sign up'
+    expect(page).to have_content('Create Profile')
+    fill_in 'profile_about', with: 'Im a dog!'
+    click_button 'Create Profile'
+    click_on 'Sign Out'
 
+    visit '/users/sign_up'
+    fill_in 'user[name]', with: 'Kitty'
+    fill_in 'user[surname]', with: 'TheCat'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: '123456'
+    fill_in 'user[password_confirmation]', with: '123456'
+    click_button 'Sign up'
+    expect(page).to have_content('Create Profile')
+    fill_in 'profile_about', with: 'Im a cat!'
+    click_button 'Create Profile'
+    visit '/'
+    visit '/users/profiles'
+    expect(page).to have_content('User Profiles')
+    click_on 'Kit TheDog'
+    expect(page).to have_content('Im a dog!')
   end
 
   scenario 'Upon clicking, shows the details of their profile and posts' do
-    
+    visit '/users/sign_up'
+    fill_in 'user[name]', with: 'Kit'
+    fill_in 'user[surname]', with: 'TheDog'
+    fill_in 'user[email]', with: 'testing@test.com'
+    fill_in 'user[password]', with: '123456'
+    fill_in 'user[password_confirmation]', with: '123456'
+    click_button 'Sign up'
+    expect(page).to have_content('Create Profile')
+    fill_in 'profile_about', with: 'Im a dog!'
+    click_button 'Create Profile'
+    visit '/posts'
+
+    fill_in 'post[message]', with: 'Hello, world!'
+    click_on 'Post'
+    visit '/posts'
+    expect(page).to have_content('Hello, world!')
+    click_on 'Sign Out'
+
+    visit '/users/sign_up'
+    fill_in 'user[name]', with: 'Kitty'
+    fill_in 'user[surname]', with: 'TheCat'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: '123456'
+    fill_in 'user[password_confirmation]', with: '123456'
+    click_button 'Sign up'
+    expect(page).to have_content('Create Profile')
+    fill_in 'profile_about', with: 'Im a cat!'
+    click_button 'Create Profile'
+    visit '/'
+    visit '/users/profiles'
+    expect(page).to have_content('User Profiles')
+    click_on 'Kit TheDog'
+    expect(page).to have_content('Im a dog!')
+    expect(page).to have_content('Bark!')
   end 
 end
