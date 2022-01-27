ddfe8a8b4b782494a85f78dd206c045fb3ee9e89
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-01-27 10:34:00 +0000

    Remove duplicate logout route

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   
   devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-    
     authenticated :user do
       # p "We're in the right place"
       # @user = User.find(current_user.id)
