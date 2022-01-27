9824de7a958a405d83b00506a3c98f8a1f7f9da1
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-01-27 14:16:17 +0000

    Add notification count to navbar icon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :count_notifications
   protect_from_forgery with: :exception
+
+  private
+  def count_notifications
+    @notification_count = Notification.count
+  end
 end
