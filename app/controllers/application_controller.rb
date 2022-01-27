65ab50bb1eac15c504a295c714346f2bc378fb46
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-01-27 16:10:29 +0000

    Fix notification count icounting all notifications in table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,10 @@
 
 class ApplicationController < ActionController::Base
   before_action :count_notifications
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true
 
   private
   def count_notifications
-    @notification_count = Notification.count
-    # @notification_count = current_user.notifications.count
+      @notification_count = current_user.notifications.count if user_signed_in? 
   end
 end
