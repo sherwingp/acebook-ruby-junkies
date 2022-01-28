b974c511c454f9cbfd2e3099a37ac0ee59aa73c1
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-01-28 12:02:00 +0000

    Fix notification not being destroyed on friend request acceptance

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -6,6 +6,9 @@ class FriendRequest < ApplicationRecord
   validate :not_friends
   validate :not_pending
 
+  has_many :notifications, as: :recipient, dependent: :destroy
+  has_noticed_notifications param_name: :parent, destroy: true, model_name: "Notification"
+
   def accept
     user.friends << friend
     destroy
@@ -33,6 +36,6 @@ class FriendRequest < ApplicationRecord
     recipient = User.find(friend.id)
     sender = User.find(user_id)
     requester = "#{sender.name} #{sender.surname}"
-    FriendRequestNotification.with(message: self, sender: sender, requester: requester).deliver_later(recipient) unless recipient == sender
+    FriendRequestNotification.with(parent: self, message: self, sender: sender, requester: requester).deliver_later(recipient) unless recipient == sender
   end
 end
