6f93d1c52b57f37b8404c919c5f5aa955f0d66f0
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-01-28 11:51:35 +0000

    Write tests for friend request notifications

--- spec/features/notification-tests/notification_spec.rb
+++ spec/features/notification-tests/notification_spec.rb
@@ -81,4 +81,53 @@ RSpec.feature "Notifications", type: :feature do
     click_button('Remove all notifications')
     expect(page).to_not have_content("You have a new comment")
   end
+
+  scenario "a user sees a notification after receiving a friend request" do
+    # User 1 comments on user 2's post
+    visit '/'
+    fill_in 'profile_about', with: 'Im a cat!'
+    click_button 'Create Profile'
+    logout(@user1)
+
+    # User 2 adds user 1
+    login_as(@user2, :scope => :user)
+    visit '/'
+    fill_in 'profile_about', with: 'Im a dog!'
+    click_button 'Create Profile'
+    visit '/users/profiles'
+    click_button 'Add Friend'
+    logout(@user2)
+
+    # # User 1 sees a notification
+    login_as(@user1, :scope => :user)
+    visit "/users/#{@user2.id}/notifications"
+    expect(page).to have_content("You have a new friend request")
+  end
+
+  scenario "a user sees no notification after accepting a friend request" do
+    # User 1 comments on user 2's post
+    visit '/'
+    fill_in 'profile_about', with: 'Im a cat!'
+    click_button 'Create Profile'
+    logout(@user1)
+
+    # User 2 adds user 1
+    login_as(@user2, :scope => :user)
+    visit '/'
+    fill_in 'profile_about', with: 'Im a dog!'
+    click_button 'Create Profile'
+    visit '/users/profiles'
+    click_button 'Add Friend'
+    logout(@user2)
+
+    # User 1 sees a notification and accepts
+    login_as(@user1, :scope => :user)
+    visit "/users/#{@user1.id}/notifications"
+    click_on('You have a new friend request from name surname.')
+    click_button('Accept')
+
+    # User 1 sees no notification
+    visit "/users/#{@user1.id}/notifications"
+    expect(page).to_not have_content("You have a new friend request")
+  end
 end
